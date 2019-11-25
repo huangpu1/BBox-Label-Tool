181c9898d7b4d4c269a043992bd587108171ab22
pu.huang <33538745+huangpu1@users.noreply.github.com>
2019-11-25 17:20:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@
 #
 #-------------------------------------------------------------------------------
 from __future__ import division
-from Tkinter import *
-import tkMessageBox
+from tkinter import *
+
+# import tkMessageBox
 from PIL import Image, ImageTk
 import os
 import glob
@@ -123,17 +124,20 @@ class LabelTool():
         if not dbg:
             s = self.entry.get()
             self.parent.focus()
-            self.category = int(s)
+            # self.category = int(s)
         else:
             s = r'D:\workspace\python\labelGUI'
 ##        if not os.path.isdir(s):
 ##            tkMessageBox.showerror("Error!", message = "The specified dir doesn't exist!")
 ##            return
         # get image list
-        self.imageDir = os.path.join(r'./Images', '%03d' %(self.category)) # put your data into another 002 folder
-        self.imageList = glob.glob(os.path.join(self.imageDir, '*.jpg')) #.JPEG
+        # self.imageDir = os.path.join(r'./Images/', '%03d' %(self.category)) # put your data into another 002 folder
+        self.imageDir = os.path.join(r'./Images/001/') # put your data into another 002 folder
+
+        print (self.imageDir)
+        self.imageList = glob.glob(os.path.join(self.imageDir, '*.JPEG')) #.JPEG
         if len(self.imageList) == 0:
-            print 'No .JPEG images found in the specified dir!'
+            print ('No .JPEG images found in the specified dir!')
             return
 
         # default to the 1st image in the collection
@@ -164,7 +168,7 @@ class LabelTool():
         #    self.egLabels[i].config(image = self.egList[-1], width = SIZE[0], height = SIZE[1])
 
         self.loadImage()
-        print '%d images loaded from %s' %(self.total, s)
+        print ('%d images loaded from %s' %(self.total, s))
 
     def loadImage(self):
         # load image
@@ -203,7 +207,7 @@ class LabelTool():
             f.write('%d\n' %len(self.bboxList))
             for bbox in self.bboxList:
                 f.write(' '.join(map(str, bbox)) + '\n')
-        print 'Image No. %d saved' %(self.cur)
+        # print ('Image No. %d saved' %(self.cur))
 
 
     def mouseClick(self, event):
